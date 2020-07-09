89739bf6d728c72eac0f626e3e6a7209f7953e7e
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2020-07-09 16:02:10 -0400

    Updates

---
+++ precache-manifest.37846ddfd6cc8ffaacdfaf7421e84be7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c824bb0a4e61f20d68910e7f1b0d776d",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "680f4cf380ae372a86ba",
+    "url": "/reactportfolio/static/css/main.b06775a1.chunk.css"
+  },
+  {
+    "revision": "1e53b8f609b78b490e06",
+    "url": "/reactportfolio/static/js/2.44727926.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.44727926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680f4cf380ae372a86ba",
+    "url": "/reactportfolio/static/js/main.7d75ee45.chunk.js"
+  },
+  {
+    "revision": "031c1cfc0d933da9b771",
+    "url": "/reactportfolio/static/js/runtime-main.a3b9637f.js"
+  },
+  {
+    "revision": "cf706cd00a81acf9897ff10eb2b179c1",
+    "url": "/reactportfolio/static/media/Taskit.cf706cd0.jpeg"
+  },
+  {
+    "revision": "1d22aac62315716e36fe3783cd88520f",
+    "url": "/reactportfolio/static/media/demo.1d22aac6.png"
+  },
+  {
+    "revision": "4796953fc4fa8bf869ce665c93736bd3",
+    "url": "/reactportfolio/static/media/noteTaker.4796953f.jpeg"
+  },
+  {
+    "revision": "4a748f34327f7024ce6d5e335cbdd6a9",
+    "url": "/reactportfolio/static/media/scheduler.4a748f34.png"
+  },
+  {
+    "revision": "e3d3c53e803d01f735031e75d3f3b9ec",
+    "url": "/reactportfolio/static/media/vocabulary.e3d3c53e.jpeg"
+  },
+  {
+    "revision": "ce98b3c327a880463e160955d07f26df",
+    "url": "/reactportfolio/static/media/wendyportfolio.ce98b3c3.jpeg"
+  }
+]);
